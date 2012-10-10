aaee52e25358b0771fac9c0472437a672e5c3bc0
Aakash Shah <ashah@mines.edu>
2012-10-10 11:29:19 -0600

    Finished classes that represent a cell on the board & the BadConfigFormatException

--- ClueGame/RoomCell.java
+++ ClueGame/RoomCell.java
@@ -1,5 +1,32 @@
 package ClueGame;
 
-public class RoomCell {
+public class RoomCell extends BoardCell {
+
+	enum DoorDirection {
+		UP, DOWN, LEFT, RIGHT;
+	}
+	
+	private DoorDirection doorDirection;
+	private char roomInitial;
+	
+	@Override
+	public boolean isWalkaway() {
+		return false;
+	}
+
+	@Override
+	public boolean isRoom() {
+		return true;
+	}
+
+	@Override
+	public boolean isDoorway() {
+		return false;
+	}
+
+	@Override
+	public void draw() {
+		
+	}
 
 }
