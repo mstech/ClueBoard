7268039c2f4eeecde3268be73755fbaa6b2901ad
Matthew Stech <mstech@mymail.mines.edu>
2012-10-11 10:31:38 -0600

    updated the Board to have all necissary functions

--- ClueGame/Board.java
+++ ClueGame/Board.java
@@ -1,7 +1,39 @@
 package ClueGame;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 public class Board {
-	ArrayList<BoardCell> cells; 
+		private ArrayList<BoardCell> cells;
+		private Map<Character, String> rooms;
+		private int numRows;
+		private int numColumns;
+		
+		public void LoadConfigFiles() {
+			
+		}
+		
+		public int calcIndex(int row, int col) {
+			return 0;
+		}
+		
+		public RoomCell getRoomCellAt(int row, int col) {
+			return null;
+		}
+
+		public ArrayList<BoardCell> getCells() {
+			return cells;
+		}
+
+		public Map<Character, String> getRooms() {
+			return rooms;
+		}
+
+		public int getNumRows() {
+			return numRows;
+		}
+
+		public int getNumColumns() {
+			return numColumns;
+		}
 }
